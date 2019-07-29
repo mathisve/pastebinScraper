2c392644b8dab33988ddcd6572fb12cc2db12fd0
Mathis Van Eetvelde <mathis.vaneetvelde@gmail.com>
2019-07-29 04:12:24 +0200

    Added link_history & history_depth

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
 import time
-from bs4 import BeautifulSoup
 import requests
+
+from bs4 import BeautifulSoup
+
 from tqdm import tqdm
 
 pastebin_public_page = "https://pastebin.com/archive"
 save_folder = "/bins"
+
+history_depth = 5
 
 
 def scrapeArchive(link_history):
@@ -18,14 +22,25 @@ def scrapeArchive(link_history):
 			try:
 				maintable = content.find("table", {"class":"maintable"})
 				for link in maintable.find_all("a"):
-						sLink = str(link).split('"') #split link in array with " delimiter to get the link part
+						sLink = str(link).split('"')
 						nLink = "https://pastebin.com/raw" + sLink[1]
-						if(nLink not in link_history):
+
+						in_history = False
+						for x in link_history:
+							if(nLink in x):
+								in_history = True
+								break
+
+						if(in_history == False):		
 							usable_links.append(nLink)
+
 			except Exception as e:
 				print(e)
 
-			link_history = usable_links
+			if(len(usable_links) != 0):
+				link_history.append(usable_links)
+				if(len(link_history) >= history_depth):
+					link_history.pop(0)
 
 		else:
 			print("Content type is None, something is wrong!")
@@ -40,24 +55,30 @@ def main():
 	iterations = 0
 
 	while True:
-		folder_dir = os.path.join(os.getcwd(), str(int(time.time())))
-		os.mkdir(folder_dir)
-
 		iterations += 1
 		links, link_history = scrapeArchive(link_history)
-		sleep_time = 120/len(links)
-		
-		print("{} iterations. {}sec per iteration - {}".format(iterations, round(sleep_time, 2), time.asctime()))
-		for link in tqdm(links):
-			try:
-				response = requests.get(link)
-				with open(os.path.join(folder_dir, link.split("/")[-1] + ".txt"), "w") as file:
-					file.write(str(response.content).strip())
-					file.close()
-			except:
-				pass
+		if(len(links) != 0):
 
-			time.sleep(sleep_time)
+			folder_dir = os.path.join(os.getcwd(), str(int(time.time())))
+			os.mkdir(folder_dir)
+
+			sleep_time = 60/len(links)
+		
+			print("{} iterations. {}sec per iteration. {} new links found - {}".format(iterations, round(sleep_time, 2),len(links), time.asctime()))
+			for link in tqdm(links):
+				try:
+					response = requests.get(link)
+					with open(os.path.join(folder_dir, link.split("/")[-1] + ".txt"), "w") as file:
+						file.write(str(response.content).strip())
+						file.close()
+				except:
+					pass
+
+				time.sleep(sleep_time)
+
+		else:
+			print("{} iterations. No new links found, sleeping ...".format(iterations))
+			time.sleep(30)
 
 
 if __name__ == "__main__":
